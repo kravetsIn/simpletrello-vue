7086289530158a118e172acadd077226548e2a0a
Vladyslav Kravets <hello.kravets@gmail.com>
2020-12-19 13:45:59 +0200

    init commit

---
+++ src/default-board.js
@@ -0,0 +1,52 @@
+import { uuid } from './utils'
+
+export default {
+  name: 'workshop',
+  columns: [
+    {
+      name: 'todo',
+      tasks: [
+        {
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+          name: 'first task',
+          id: uuid(),
+          userAssigned: null
+        },
+        {
+          description: '',
+          name: 'second task',
+          id: uuid(),
+          userAssigned: null
+        },
+        {
+          description: '',
+          name: 'and thrid',
+          id: uuid(),
+          userAssigned: null
+        }
+      ]
+    },
+    {
+      name: 'in-progress',
+      tasks: [
+        {
+          description: '',
+          name: 'first task',
+          id: uuid(),
+          userAssigned: null
+        }
+      ]
+    },
+    {
+      name: 'done',
+      tasks: [
+        {
+          description: '',
+          name: 'first task',
+          id: uuid(),
+          userAssigned: null
+        }
+      ]
+    }
+  ]
+}
---
+++ src/store.js
@@ -0,0 +1,42 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import defaultBoard from './default-board'
+import { saveStatePlugin, uuid } from '@/utils'
+
+Vue.use(Vuex)
+
+const board = JSON.parse(localStorage.getItem('board')) || defaultBoard
+
+export default new Vuex.Store({
+  plugins: [saveStatePlugin],
+  state: {
+    board
+  },
+  mutations: {
+    CREATE_TASK (state, { tasks, name }) {
+      tasks.push({
+        name,
+        id: uuid(),
+        description: ''
+      })
+    },
+    UPDATE_TASK (state, { task, key, value }) {
+      task[key] = value
+      // Vue.set(task, key, value)
+    },
+    MOVE_TASK (state, {}) {}
+  },
+  getters: {
+    getTask (state) {
+      const getTaskByID = (id) => {
+        for (const column of state.board.columns) {
+          for (const task of column.tasks) {
+            if (task.id === id) return task
+          }
+        }
+      }
+
+      return getTaskByID
+    }
+  }
+})
