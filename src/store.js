5ded4045b2d5143e6905aadbd50759c8678bdf43
Vladyslav Kravets <hello.kravets@gmail.com>
2020-12-20 12:39:03 +0200

    addded drag and drop tasks and columns

--- src/store.js
+++ src/store.js
@@ -24,19 +24,26 @@ export default new Vuex.Store({
       task[key] = value
       // Vue.set(task, key, value)
     },
-    MOVE_TASK (state, {}) {}
+    MOVE_TASK (state, { fromTasks, toTasks, fromTaskIndex, toTaskIndex }) {
+      const taskToMove = fromTasks.splice(fromTaskIndex, 1)[0]
+      toTasks.splice(toTaskIndex, 0, taskToMove)
+    },
+    MOVE_COLUMN (state, { fromColumnIndex, toColumnIndex }) {
+      const columnList = state.board.columns
+
+      const columnToMove = columnList.splice(fromColumnIndex, 1)[0]
+      columnList.splice(toColumnIndex, 0, columnToMove)
+    }
   },
   getters: {
     getTask (state) {
-      const getTaskByID = (id) => {
+      return (id) => {
         for (const column of state.board.columns) {
           for (const task of column.tasks) {
             if (task.id === id) return task
           }
         }
       }
-
-      return getTaskByID
     }
   }
 })
